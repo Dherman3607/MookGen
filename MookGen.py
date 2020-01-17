f2b6b7cf89a3620298603982cbd38df76d75a974
David <David@home.nc>
2020-01-16 21:27:13 -0500

    working attribute distribution

--- MookGen.py
+++ MookGen.py
@@ -23,6 +23,7 @@ Stats = {
     'Initiative': 0
 
 }
+maxStats = {}
 
 
     #'Initiative': Stats['Physical']['Reaction'] + Stats['Mental']['Intelligence']
@@ -31,8 +32,43 @@ def ReadJson(Inputfile):
 
     with open(Inputfile,'r') as f:
         return json.load(f)
-def  addAttributes(inputStats,SpendablePoints):
-    print(inputStats,SpendablePoints)
+
+def  addAttributes(inputStats,maxStats,SpendablePoints,SpendableSpecialPoints):
+    print('inside')
+    print(inputStats,SpendablePoints,SpendableSpecialPoints)
+    NormalStats = ['Body','Agility','Reaction','Strength','Willpower','Logic','Charisma','Intelligence']
+    advancedStats = ['Magic','Resonance','Edge']
+    for stat in advancedStats:
+        print(stat)
+        if(inputStats[stat] == "-"):
+            print("character doesn't have this stat, remove")
+            advancedStats.remove(stat)
+
+    while SpendablePoints > 0:
+        increaseStat = random.choice(NormalStats)
+        if(inputStats[increaseStat]< maxStats[increaseStat]):
+            print('increasing stat?')
+            inputStats[increaseStat]+=1
+            SpendablePoints -= 1
+        else:
+            print("this stat is at it's max?")
+            NormalStats.remove(increaseStat)
+
+    while SpendableSpecialPoints > 0:
+        if(advancedStats):
+            increaseStat = random.choice(advancedStats)
+            print(increaseStat)
+            if(inputStats[increaseStat] < maxStats[increaseStat]):
+                inputStats[increaseStat] += 1
+                SpendableSpecialPoints -= 1
+            else:
+                advancedStats.remove(increaseStat)
+        else:
+            break
+    return inputStats
+
+
+    print(inputStats)
 
 def MakeEasyMook():
     sumToTen = 10
@@ -64,11 +100,12 @@ def MakeEasyMook():
 
 
     for stat in metatypeStats[metatype]:
+
         if stat == 'Special':
             Stats[stat] = metatypeStats[metatype][stat]
         else:
-
             Stats[stat] = metatypeStats[metatype][stat]['starting']
+            maxStats[stat] = metatypeStats[metatype][stat]['max']
 
     attributePoints = int(PointCost[0]['attributes'][str(Priorities['attributes'])])
 
@@ -80,16 +117,27 @@ def MakeEasyMook():
 
         specialtyStats = SpecialStaters[str(Priorities['Magic'])][specialty]
 
-        print(type(specialty))
-        print(specialtyStats)
-        if(specialtyStats['Magic']):
-            print('we have a magic user!')
+        # print(type(specialty))
+        # print(metatype)
+        # print(specialtyStats)
+        if('Magic' in specialtyStats):
+            #print('we have a magic user!')
             Stats['Magic'] = specialtyStats['Magic']
-        elif(specialtyStats['Resonance']):
-            print('We have a technomancer!')
 
+        else:
+            Stats['Resonance'] = specialtyStats['Resonance']
+            #print('We have a technomancer!')
+        if(Stats["Special"] =='None'):
+            Stats["Special"] = specialty
+        else:
+             Stats["Special"] += Stats["Special"] + " " + specialty
+
+#    print(Stats)
+    SpendableSpecialPoints = boughtValues['metatype'][str(Priorities['metatype'])][metatype]['Specials']
+    Stats = addAttributes(Stats,maxStats,attributePoints,SpendableSpecialPoints)
+
+    print('at the end of assigning attribute points, the stat array is:')
     print(Stats)
-    Stats = addAttributes(Stats,attributePoints)
 
 
 
